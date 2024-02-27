bc3caed985d5d84f25ac4909feb647a9d6838449
Scott Suk <scottsuk0306@gmail.com>
2024-02-27 09:59:41 +0000

    Update

---
+++ examples/demo.py
@@ -0,0 +1,36 @@
+import os
+
+import streamlit as st
+from dotenv import load_dotenv
+from litellm import completion
+
+load_dotenv()
+
+st.title("ChatGPT-like clone")
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if prompt := st.chat_input("What is up?"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    with st.chat_message("assistant"):
+        response = completion(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages
+            ],
+            stream=False,
+        )
+
+        response = response.choices[0].message.content
+        st.markdown(response)
+
+    st.session_state.messages.append({"role": "assistant", "content": response})
